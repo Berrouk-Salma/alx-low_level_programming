3b36988d8a20ec2b7a4caa5b543cf34e2c1d4a1e
Berrouk-Salma <salmaberrouk4@gmail.com>
2023-06-29 15:51:16 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -47,4 +47,3 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		o += 10;
 	}
-
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,38 @@
 #include "main.h"
-/**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
-void puts2(char *str)
-{
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
 
-	while (*y != '\0')
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
 	{
-		y++;
-		longi++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
-	}
-	_putchar('\n');
+
+	return (str);
 }
